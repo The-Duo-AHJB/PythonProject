2c3299d624142b8aebf871a3c6426f8e60e5ce80
1wildman1 <1wildman1@gmail.com>
2014-11-28 19:35:54 -0500

    Some changes
    added a bunch of raw_input() because otherwise if there were letters
    involved it would blow up.

--- Mainfile.py
+++ Mainfile.py
@@ -1,11 +1,17 @@
 import mysql.connector
 
+
 def main():
 	running = True
 	login = True
+	global username #useful for using the username without reinput for command 0
 	while running== True:
-		username = input("Please enter your username\n")
-		password = input("Please enter your password\n")
+		username = raw_input("Please enter your username\n")
+		password = raw_input("Please enter your password\n")
+		database = raw_input("Please enter your schema\n")
+		host = 'COMPDBS300'
+		cnx = mysql.connector.connect(username, password, host, database)
+		cursor = cnx.cursor()
 		login = interface()
 		print(login)
 
@@ -30,7 +36,7 @@ def interface():
 	choice = input("Enter a number\n")
 	choice = int(choice)
 	if(choice == 0):
-		changePass()
+		changePass(username)
 	elif(choice == 1):
 		viewMyBid()
 	elif(choice == 2):
@@ -66,18 +72,19 @@ def interface():
 	else:
 		print("Invalid entry")
 
-def ChangePass(username):
-    print ("What do you want to change your password to?")
-    password = input()
+def changePass(username):
+        print ("What do you want to change your password to?")
+        password = raw_input() #using raw_input() instead of input() solved the issue of the name not being defined upon input of a string.
 
-    query = ("update egccuser set password = %s where username = %s")
-    qdata = (password, username)
-    try:
-            cursor.execute(query,qdata)
-            numrows = int(cursor.rowcount)
-            print(str(numrows) + " were updated")
-            cnx.commit()
-    except mysql.connector.Error as err:
-        print("Update was not successful " + str(err))
+        query = ("update egccuser set password = %s where username = %s")
+        qdata = (password, username)
+        try:
+                cursor.execute(query,qdata)
+                numrows = int(cursor.rowcount)
+                print(str(numrows) + " were updated")
+                cnx.commit()
+        except mysql.connector.Error as err:
+                print("Update was not successful " + str(err))
+        
 
 main()
