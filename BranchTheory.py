808e169c22c7eec2879a97ab4b7f4c4b4a296745
1wildman1 <1wildman1@gmail.com>
2014-12-03 17:23:04 -0500

    Added def validItemID (which checks itemID

--- BranchTheory.py
+++ BranchTheory.py
@@ -74,7 +74,7 @@ def interface(username, pword, cursor, cnx):
 		viewNumBids(cursor)
 		return True
 	elif(choice == 8):
-		viewPopItem(cursor, cnx)  #Gives the answer in tuple form.
+		viewPopItem(cursor, cnx)
 		return True
 	elif (choice == 9):
 		newItemAuction(userID, cursor, cnx)
@@ -163,35 +163,31 @@ def searchWord(cursor):
 def viewSellerRating(cursor):
         numrows = 0
         print ("What is an Item ID of the seller?")
-        qdata = input()
+        itemID = input()
         query = ("select AVG(rating) from sellerRating where sellerID = (Select sellerID from item where itemID = %s) group by sellerID")
-        cursor.execute(query,(qdata,))
+        cursor.execute(query,(itemID,))
         for rating in cursor:
                 print(str(rating[0]))
 
-        query = ("select itemID from item where itemID = %s")
-        cursor.execute(query,(qdata,))
-        for(itemID) in cursor:
-                numrows += 1
-        if numrows != 1:
-                print("The Item ID is incorrect.")
+        validItemID(cursor, itemID)
 
 
-def viewPopItem(cursor, cnx): #This needs to NOT print in tuple form. #needs the same ItemID check which can be taken from viewSellerRating
+def viewPopItem(cursor, cnx): #needs to print ALL the attributes.
         query = ("select itemID from(select count(*) as nbids, itemID from Bid group by (itemID)) bidsperitem where nbids >= all(select count(itemid) as nbids from bid group by itemid)")
         cursor.execute(query)
-        for (itemID) in cursor:
-                print(itemID)      
+        for itemID in cursor:
+                print(itemID[0])      
 
-def shipItem(cursor, cnx): 
+def shipItem(cursor, cnx): #Needs errors.  Needs to check if the user is allowed to change it to shipped.
         print("What is the Item ID of the item you have shipped?")
-        qdata = input()
+        itemID = input()
         try:
+                validItemID(cursor, itemID)
                 query = ("update item set status = 'shipped' where itemid = %s")
-                cursor.execute(query,(qdata,))
+                cursor.execute(query,(itemID,))
                 cnx.commit()
                 query = ("update purchase set dateShipped = date(sysdate()) where itemid = %s")
-                cursor.execute(query,(qdata,))
+                cursor.execute(query,(itemID,))
                 cnx.commit()
                 print("Item status changed to shipped successfully!")
         except mysql.connector.Error as err:
@@ -211,6 +207,16 @@ def searchCata(cursor):
 	cursor.execute(query, (word,))
 	for (ItemID, title, description, startingBid, highestBid, endDate, SellerID, status, category) in cursor:
 		print(str(ItemID) + "\t" + str(title) + "\t"+str(description)+"\t"+str(startingBid)+"\t"+str(highestBid)+"\t"+str(endDate)+"\t"+str(SellerID)+"\t"+str(status)+"\t"+str(category) +"\n")
+
+def validItemID(cursor, itemID):
+        query = ("select itemID from item where itemID = %s")
+        cursor.execute(query, (itemID,))
+        numrows = 0
+        for(itemID) in cursor:
+                numrows =+1
+
+        if numrows != 1:
+                print("The Item ID is incorrect.")
 	
 def viewNumBids(cursor):
 	query = ("select count(currentbid) itemNum from bid where itemid = %s group by itemid")
@@ -218,6 +224,8 @@ def viewNumBids(cursor):
 	cursor.execute(query,(itemID,))
 	for itemNum in cursor:
 		print(str(itemNum[0]))
+
+	validItemID(cursor, itemID)
 
 def newItemAuction(userID, cursor, cnx):
 	query = ("Select max(ItemID) maxNum from item")
@@ -240,17 +248,19 @@ def newItemAuction(userID, cursor, cnx):
 	cnx.commit()
 	print("Item listed successfully!")
 
-def viewHighBid(cursor): #needs the error handle like above
+def viewHighBid(cursor):
 	query = ("select max(currentbid) num from bid where itemid = %s group by itemid")
 	itemID = input("Enter the id of the item for which you wish to know the highest current bid\n")
 	try:
 		cursor.execute(query, (itemID,))
 		for num in cursor:
 			print(num[0])
-	except:
-		print("Invalid item ID")
+	except mysql.connector.Error as err:
+		print("Viewing the highest bid was unsuccessful" + str(err))
+
+	validItemID(cursor, itemID)
 		
-def placeBid(userID, cursor, cnx):
+def placeBid(userID, cursor, cnx): #needs error message if status isn't open OR if the bid isn't higher than highest bid
         print("What is the Item ID of the item you want to bid on?")
         itemID = input()
         print("How much would you like to bid?")
@@ -262,9 +272,11 @@ def placeBid(userID, cursor, cnx):
                 cnx.commit()
                 print("You have bid successfully!")
         except mysql.connector.Error as err:
-                print("Bid was not successful " + str(err))        
+                print("Bid was not successful " + str(err))
 
-def rateSeller(userID, cursor, cnx): #I think this works.  It makes sense for it to be limited so that the buyer can only rate the seller once we need time added for flexibility
+        validItemID(cursor, itemID)
+
+def rateSeller(userID, cursor, cnx): #needs error message if sellerID is not valid ID for a seller
         print("What is the seller ID?")
         seller = input()
         print("What rating do you want to give the seller?")
@@ -278,20 +290,22 @@ def rateSeller(userID, cursor, cnx): #I think this works.  It makes sense for it
         except mysql.connector.Error as err:
                 print("Rating was not successful " + str(err))
 
-def closeAuction(userID, cursor, cnx):
+def closeAuction(userID, cursor, cnx): #needs to error if item has already been sold
         print("What is the Item ID of the auction you want to close?")
-        ItemID = input()
+        itemID = input()
         query = ("insert into purchase values ((select buyerid from (select max(currentbid), buyerid from bid where itemid = %s group by itemid) FinalBidder), %s, (select highestbid from item where itemid = %s), date(sysdate()), null)")
-        qdata = (ItemID, ItemID, ItemID)
+        qdata = (itemID, itemID, itemID)
         try:
                 cursor.execute(query, qdata)
                 cnx.commit()
                 query = ("update item set status = 'sold' where itemid = %s")
-                cursor.execute(query, (ItemID,))
+                cursor.execute(query, (itemID,))
                 cnx.commit()
                 print("You have successfully closed the auction.")
         except mysql.connector.Error as err:
                 print("Closing the auction was unsuccessful " + str(err))
+
+        validItemID(cursor, itemID)
 
 def viewTopSellers(cursor):
         query = ("select sellerid, avg(rating) as rating from sellerrating group by sellerid order by avg(rating) desc")
